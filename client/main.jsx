e5641303d45f0d806e87b836dd8f918aac8af30d
garrilla (Surface) <garry@kinicho.com>
2023-05-17 10:13:54 +0100

    client side code

--- client/main.jsx
+++ client/main.jsx
@@ -3,12 +3,13 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
 import '/imports/api/deviceMethods';
+import '/imports/api/messagesMethods';
 import OSC from 'osc-js';
 
 
 Meteor.startup(() => {
   render(<App/>, document.getElementById('react-target'));
-  Meteor.subscribe('devices')
-  Meteor.subscribe('scenes')
-  //Meteor.call('deviceUpdate',"fGXsSmgrB434Hszgq","awesome");
+  Meteor.subscribe('devices');
+  Meteor.subscribe('messages');
+  Meteor.call('resetAllDevices',"fGXsSmgrB434Hsgaz","awesome");
 });
